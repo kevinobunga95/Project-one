c9a6f179b8018dabc808561d6431e988ea093dc4
kevinobunga95 <kevinobunga95@gmail.com>
2024-07-29 06:35:09 -0400

    displaying various datatypes

---
+++ Datatypes.py
@@ -0,0 +1,38 @@
+my_name = "kevin obunga"
+print(my_name)
+print(type(my_name))
+
+Age = 24
+print(Age)
+print(type(Age))
+
+my_GPA = 23.35
+print(my_GPA)
+print(type(my_GPA))
+
+my_list = ["Kevin", "Mike", "Steve", "kevin"]
+print(my_list[0:4])
+print(type(my_list))
+
+my_tuple = (1, 2, 3, 4, 5)
+print(my_tuple)
+print(type(my_tuple))
+
+my_range = range(10)
+print(list(my_range))
+print(type(my_range))
+
+my_dictionary = {"name": "kevin Obunga", "age": 24}
+print(my_dictionary)
+print(type(my_dictionary))
+
+food_types = {"rice", "beans", "kales"}
+print(food_types)
+print(type(food_types))
+
+today_is_monday = True
+tomorrow_is_monday = False
+print(today_is_monday)
+print(tomorrow_is_monday)
+
+print(type(today_is_monday))
