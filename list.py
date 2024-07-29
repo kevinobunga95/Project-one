f59c9c433b77c1d53e26f048e4c70615646c2cd3
kevinobunga95 <kevinobunga95@gmail.com>
2024-07-29 07:13:59 -0400

    Learning how to iterate over list

---
+++ list.py
@@ -0,0 +1,13 @@
+favourite_fruits = ["mango", "pineapple", "apples", "passion", "oranges", "bananas", "Melon"]
+print(favourite_fruits)
+print(favourite_fruits[5])
+print(favourite_fruits[-1])
+
+favourite_fruits.remove(favourite_fruits[-1])
+print(favourite_fruits)
+
+favourite_fruits.append("grapes")
+print(favourite_fruits)
+
+favourite_fruits.insert(0,"ovacado")
+print(favourite_fruits)
